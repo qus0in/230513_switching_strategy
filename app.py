12257dd70e982c8721a0c0c644e48bc093957247
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-13 03:57:48 +0000

    first

--- app.py
+++ app.py
@@ -91,7 +91,12 @@ def visualize(cd, period, weight, span):
     st.plotly_chart(fig, True, theme=None)
     # return df.iloc[-1]
 
-    st.write(table.index[0], table.index[1])
+    # 이중축
+    # st.write(table.index[0], table.index[1])
+    first, second = table.index[:2]
+    st.write(cd.data[first])
+
+
 
 data = History()
 PERIOD = 20
